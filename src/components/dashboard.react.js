51f2f859891f6139cb01a702cfe3a2443593e6a5
Nardier <nardierbarbosa@gmail.com>
2019-07-20 01:37:07 -0300

    Initialize components

--- src/components/dashboard.react.js
+++ src/components/dashboard.react.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Chart from "react-apexcharts";
+import Panel from "./panel.react";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -53,12 +54,7 @@ class Dashboard extends Component {
                 </div>
               </div>
               <div className="row">
-                <div className="col-lg-3 col-md-6 col-sm-6">
-                  <div className="panel panel-default b-a-0 shadow-box">
-                    <div className="panel-heading">Main Fundings</div>
-                    <div className="panel-body"><h2 className="m-t-0 f-w-300"><sup>$</sup> <span> 706.00</span></h2> <i className="fa fa-fw fa-caret-up text-success"></i> <span>$ 871.00</span></div>
-                  </div>
-                </div>
+              <Panel />
                 <div className="col-lg-3 col-md-6 col-sm-6">
                   <div className="panel panel-default b-a-0 shadow-box">
                     <div className="panel-heading">Invoices</div>
