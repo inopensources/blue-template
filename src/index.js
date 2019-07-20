cb37ac88d8656f80b5b78f26187e99d58856f05a
Armando Couto <coutoarmando@gmail.com>
2019-07-19 23:00:40 -0300

    Corrige uma parte do login

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
-import './index.css';
+import Signin from './components/signin.react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Signin />, document.getElementById('root'));
