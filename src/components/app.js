f8d2dcd451b5eee5215ddf28d1daa6db89820e8b
marcelfonteles <marcelfonteles@gmail.com>
2019-07-19 22:46:40 -0300

    inserindo template

--- src/components/app.js
+++ src/components/app.js
@@ -15,7 +15,20 @@ class App extends React.Component {
         } = this.props;
 
         return (
-          <button className="btn btn-primary">Botão</button>
+            <div>
+                <button className="btn btn-primary" type="button">aaa</button>
+                <div className="col-lg-3 col-md-6 col-sm-6">
+    <div className="panel panel-default b-a-0 shadow-box">
+        <div className="panel-heading">Main Fundings</div>
+            <div className="panel-body">
+                <h2 className="m-t-0 f-w-300"><sup>$</sup>
+                    <span> 901.00</span>
+                </h2>
+                <i className="fa fa-fw fa-caret-up text-success"></i> <span>$ 137.00</span>
+            </div>
+        </div>
+    </div>
+    </div>
         );
     }
 }
