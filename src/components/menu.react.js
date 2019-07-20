241b688b618f1ec23f4ed18dfde7fb81f0127bff
RayaneMarusca <maruscarayane@gmail.com>
2019-07-20 01:38:34 -0300

    merge

--- src/components/menu.react.js
+++ src/components/menu.react.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const menu = (props) => (
-	<div></div>
-);
+class Menu extends Component {
+    render() {
+        return (
+            <div className="navbar-inverse navbar navbar-fixed-top">
+                <div className="container-fluid">
+        
+                    <div className="navbar-header">
+                    </div>
+       
+                    <div className="collapse navbar-collapse" id="navbar">
+                        <ul className="nav navbar-nav navbar-left clearfix yamm">
 
-menu.propTypes = {};
+                        </ul>
+            
+                        <ul className="nav navbar-nav navbar-right">
+<div className="container">
+    <div className="row">
+        <div className="col-lg-12">
 
-export default menu;
+           
+
+        </div>
+    </div>
+</div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+
+
+export default Menu;
