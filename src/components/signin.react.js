177ebbea7dfd157b28fd58589e834e65b5912ed3
Nardier <nardierbarbosa@gmail.com>
2019-07-19 22:34:36 -0300

    Add content to component

--- src/components/signin.react.js
+++ src/components/signin.react.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const signin = (props) => (
 	<div>
-	  { /* JSX */ }
+	  <body>
+		  <div>
+			  <div class="form-group">
+					<input type="text" placeholder="Email: " class="form-control" id="email"></input>
+					<input type="password" placeholder="Senha: " class="form-control" id="password"></input>
+					<button id="signUpBtn">Sign Up</button>
+					<button id="signInBtn">Sign In</button>
+			  </div>
+		  </div>
+	  </body>
 	</div>
 );
 
