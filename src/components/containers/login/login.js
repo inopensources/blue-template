a1bec97748f7c38ac634ae84b2e4f231a42ba722
Nardier <nardierbarbosa@gmail.com>
2019-07-19 22:12:25 -0300

    Trying put firebase + electron email sign up/sign in

--- src/components/containers/login/login.js
+++ src/components/containers/login/login.js
@@ -23,3 +23,37 @@ class Login extends Component {
 }
 
 export default Login;
+var config = {
+        apiKey: "AIzaSyCrH6D9SX_Jvn723Ce_4hXKe0BjmvrtC7Q",
+        authDomain: "blue-template-247300.firebaseapp.com",
+        databaseURL: "https://blue-template-247300.firebaseio.com",
+        projectId: "blue-template-247300",
+        storageBucket: "",
+        messagingSenderId: "357730226695",
+        appId: "1:357730226695:web:a9a28732de7ee660"
+    };
+
+
+firebase.initializeApp(config);
+
+var signUpBtn = document.getElementById('signUpBtn');
+var signInBtn = document.getElementById('signInBtn');
+
+signInBtn.addEventListener('click',function(){
+    var emailField = document.getElementById('email').value;
+    var passwordField = document.getElementById('password').value;
+
+    firebase.auth().createUserWithEmailAndPassword(emailField, passwordField).catch(function(error){
+        if(error != null){
+            console.log(error.message);
+            return;
+        }
+        alert('User Created !!')
+    })
+}); 
+
+signInBtn.addEventListener('click', function(){
+
+    var emailField = document.getElementById('email').value;
+    var passwordField = document.getElementById('password').value;
+});
