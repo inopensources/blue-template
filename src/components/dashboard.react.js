f9345dfa5057dc5d44eab9b746fd322322ab5c26
Nardier <nardierbarbosa@gmail.com>
2019-07-20 02:43:34 -0300

    Component chart

--- src/components/dashboard.react.js
+++ src/components/dashboard.react.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Chart from "react-apexcharts";
+import Charts from "./Chart.react";
 import Footer from "./footer.react";
 import Menu from "./menu.react";
 import Header from "./header.react";
@@ -7,23 +7,6 @@ import Header from "./header.react";
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      options: {
-        chart: {
-          id: "basic-bar"
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-        }
-      },
-      series: [
-        {
-          name: "series-1",
-          data: [30, 40, 45, 50, 49, 60, 70, 91]
-        }
-      ]
-    };
   }
 
   render() {
@@ -94,12 +77,7 @@ class Dashboard extends Component {
                     <div className="panel panel-default b-gray-dark b-a-0 shadow-box">
                       <div className="panel-heading">
                         <div className="row">
-                          <Chart
-                            options={this.state.options}
-                            series={this.state.series}
-                            type="bar"
-                            width="500"
-                          />
+                          <Charts/>
                         </div>
                       </div>
                       <div className="panel-body">
